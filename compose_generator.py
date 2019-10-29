040d52dbfe5ceef3824fef7bb78a91533c87b198
root <editor>
2019-10-29 01:50:47 +0000

    initial commit

---
+++ compose_generator.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3.7
+import os
+import jinja2
+import yaml
+from pathlib import Path
+# Host_IP:        ${ifconfig | grep ens192 -b1 | grep inet | awk '{print $3}'}
+# add this to the yaml later
+
+def mkdir(service_configs):
+	try:
+		os.makedirs(service_configs)
+	except FileExistsError:
+		# directory already exists
+		pass
+with open("templates/parameters.yaml") as f:
+	config = yaml.load(f, Loader = yaml.FullLoader)
+globals = config['Globals']
+defaults = config['Defaults']
+with open('templates/shell_script.jinja.sh') as f:
+
+	shell_script = jinja2.Template(f.read())
+with open('templates/globals.jinja.env') as f:
+	global_env = jinja2.Template(f.read())
+with open('templates/service.jinja.env') as f:
+	service_env = jinja2.Template(f.read())
+with open('templates/docker-compose.jinja.yaml') as f:
+	compose_yaml = jinja2.Template(f.read())
+for path in globals['Paths']:
+	print(path, globals['Paths'][path])
+exports = []
+for i in globals['Exports']:
+	try:
+		os.makedirs(globals['Exports'][i])
+	except FileExistsError:
+		# directory already exists
+		pass
+	exports.append(f"{i}={globals['Exports'][i]}")
+for stack in config['Stack Group Name']:
+	services = config['Stack Group Name'][stack]['Services']
+	configs = f"{globals['Exports']['CONFIGS']}/{str(stack).lower()}".replace(" ", "_")
+	mkdir(configs)
+	print(f"Creating stack configuration: {configs}")
+	with open(f"{configs}/globals.env", "w+") as f:
+		f.write(global_env.render(defaults = defaults))
+	for app in services:
+		with open(f"{configs}/setup.sh", "w+") as f:
+			f.write(shell_script.render(stack_group = str(stack).lower().replace(" ", "_"),
+										service = app,
+										defaults = defaults,
+										globals = globals,
+										global_exports = exports))
+		print(f"Creating stack configuration: {configs}/setup.sh")
+		with open(f"{configs}/{app.lower()}.env", "w+") as f:
+			f.write(service_env.render(environment = (services[app]['Environment'])))
+		print(f"Creating stack configuration: {configs}/{app.lower()}.env")
+		with open(f"{configs}/docker-compose.yaml", "w+") as f:
+			f.write(compose_yaml.render(stack = (config['Stack Group Name'][stack]), defaults = defaults))
+		print(f"Creating stack configuration: {configs}/docker-compose.yaml")
---
+++ templates/shell_script.jinja.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+ssh-add /home/batman/.ssh/personal_git_rsa &wait
+docker config rm $({{ stack_group }} config list | grep -i {{ stack_group }} | awk '{print $1}')
+{%- for pair in global_exports|sort  %}
+export {{ pair }}
+{%- endfor %}
+source {{ globals['Paths']['Profile'] }}
+rebuild-symlinks &wait
+{%- if service['OAUTH_PROXY'] %}
+touch ${SECRETS}/{{ service }}/OAUTH2_PROXY_CLIENT_ID.secret
+touch ${SECRETS}/{{ service }}/OAUTH2_PROXY_CLIENT_SECRET.secret
+touch ${SECRETS}/{{ service }}/OAUTH2_PROXY_COOKIE_SECRET.secret
+{%- endif %}
+chmod 600 ${SECRETS}/*/*.secret
+chown -R  {{ defaults['PUID'] }}:{{ defaults['PGID'] }} ${SECRETS} ${CONFIGS}/{{ service }}/*
+cd ${CONFIGS}/{{ service[0]|lower }}{{ service[1:] }}
+{%- for file in global_path_cleanup|sort %}
+rm -rf {{ file }}
+{%- endfor %}
+git add .
+git commit -m "{{ service[0]|upper }}{{ service[1:] }} Current version - ${date}"
+git push &wait
+cd -
+{%- for script in globals['Paths']['Scripts'] %}
+{{ script }} &wait
+{%- endfor %}
+# these will get revised to match all paths from configs and from the list
+touch ${LOGS}/ ${CONFIGS}/traefik/acme.json
+echo "baseline data" > ${LOGS}/traefik.log
