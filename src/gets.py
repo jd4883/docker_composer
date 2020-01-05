4ae5785fca2a5f466847b2fafd78f4a279cf9065
root <editor>
2020-01-05 08:26:02 +0000

    minor corrections vpn seems to work but the web ui does not come up. Suspect i need to do some sort of nginx or something type design

--- src/gets.py
+++ src/gets.py
@@ -5,18 +5,24 @@ from src.helpers import mkdir
 
 
 def get_index(stack):
-	stack = str(stack.replace(' ', '-')).lower()
-	return stack.replace('_', '-')
+	stack = stack.replace(' ', '-')
+	stack = stack.replace('_', "-")
+	stack = stack.lower()
+	return stack
 
 
 def get_stack_file(stack):
-	return f"{os.environ['HOST_CONFIGS']}/{str(stack.replace(' ', '_')).lower()}/docker-compose.yaml"
+	stack = stack.replace(' ', '-')
+	stack = stack.replace('_', "-")
+	stack = stack.lower()
+	return f"{os.environ['HOST_CONFIGS']}/{stack}/docker-compose.yaml"
 
 
 def set_config_directory(stack):
-	stack = str(stack.replace(' ', '-')
-	stack = str(stack).replace('_', "-")
-	configs = f"{os.environ['CONFIG']}/{stack).lower()}"
+	stack = stack.replace(' ', '-')
+	stack = stack.replace('_', "-")
+	stack = stack.lower()
+	configs = f"{os.environ['CONFIG']}/{stack}"
 	mkdir(configs)
 	print(f"Creating stack configuration: {configs}")
 	return configs
