de85942579de1ac634d3d1bd96649f3398195392
root <editor>
2020-02-03 04:42:04 +0000

    Working on factoring out the heavy dependency on jinja2 for the yaml generation. * global networks is calculated in python * version is calculated in python * backend and frontend parameters auto build form python * secrets globals block * volumes globals block * configs globals block
    * swarm vs compose generation (ideally both files can generate)

--- classes/composeParameters.py
+++ classes/composeParameters.py
@@ -1,3 +1,42 @@
+class IP(object):
+	def __init__(self,
+	             defaults = dict(),
+	             globals = dict(),
+	             stackDict = dict(),
+	             octet1 = int(10),
+	             octet2 = int(23),
+	             networkIP = int(0),
+	             externalNetworkBool = bool(True),
+	             defaultNetworkDriver = "bridge"):
+		self.subnetMask = str(int(setItems(self, "subnetMask",
+		                                   globals)))
+		self.octet1 = str(int(setItems(self, "octet1", defaults, octet1)))
+		self.octet2 = str(int(setItems(self, "octet2", defaults, octet2)))
+		self.octet3 = str(int(setItems(self, "octets", stackDict)[0]))
+		self.networkIP = str(networkIP)
+		# 4th octet is dynamically calculated for each app
+		self.octetsList = [
+				str(self.octet1),
+				str(self.octet2),
+				str(self.octet3)
+				]
+		self.address = formatString(self, ".".join(self.octetsList))
+		self.backendSubnet = self.parseBackendSubnet()
+		self.networks = { "networks": dict() }
+		self.networks["networks"].update({ "frontend": { "external": externalNetworkBool } })
+		self.networks["networks"].update({ "backend": dict() })
+		self.networks["networks"]["backend"].update({ "driver": defaultNetworkDriver })
+		self.networks["networks"]["backend"].update({ "ipam": dict() })
+		self.networks["networks"]["backend"]["ipam"].update({ "config": list() })
+		self.networks["networks"]["backend"]["ipam"]["config"].append({ "subnet": self.backendSubnet })
+	
+	def parseBackendSubnet(self, networkIP = 0):
+		networkIP = networkIP
+		subnetMaskComponents = [(str(self.address) + "." + str(networkIP)), str(self.subnetMask)]
+		payload = formatString(self, "/".join(subnetMaskComponents))
+		return payload
+
+
 class ComposeFile(object):
 	def __init__(self,
 	             domain = "example.com",
@@ -11,74 +50,62 @@ class ComposeFile(object):
 	             globalValues = dict(),
 	             labels = list(),
 	             customResponseHeaders = dict(),
-	             octet1 = str(10),
-	             octet2 = str(23),
-	             subnetMask = str(24),
 	             frontendNetwork = "frontend",
 	             backendNetwork = "backend",
-	             sharedBackendNetwork = "shared-backend",
 	             authenticatedEmailsContainerPath = "/config/authenticated-emails.txt",
 	             traefikProtocol = "http",
 	             organizrSubdomain = "home",
 	             secretsPath = "${SECRETS}",
 	             oauth_port = str(4180),
 	             puid = str(1001),
-	             pgid = str(1001)):
+	             pgid = str(1001),
+	             version = 3.8):
 		self.stackDict = stackDict
 		self.defaults = defaults
 		self.externalServers = externalServers
 		self.globalValues = globalValues
-		self.subnetMask = str(int(self.setItems("subnetMask",
-		                                        self.globalValues,
-		                                        subnetMask)))
-		self.secrets = self.setItems("Secrets",
-		                             self.stackDict)
-		self.volumes = self.setItems("Volumes",
-		                             self.stackDict)
-		self.services = self.setItems("Services",
-		                              self.stackDict)
-		self.domain = str(self.setItems("Domain",
-		                                self.defaults,
-		                                domain))
-		self.email = str(self.setItems("Email",
-		                               self.defaults,
-		                               emailAddress))
-		self.octet1 = str(int(self.setItems("octet1",
-		                                    self.defaults,
-		                                    octet1)))
-		self.octet2 = str(int(self.setItems("octet2",
-		                                    self.defaults,
-		                                    octet2)))
-		self.octet3 = str(int(self.setItems("octets",
-		                                    self.stackDict)[0]))
-		# 4th octet is dynamically calculated for each app
-		self.frontendNetwork = self.formatString(self.setItems("Networks",
-		                                                       self.globalValues,
-		                                                       frontendNetwork,
-		                                                       0))
-		self.backendNetwork = self.formatString(self.setItems("Networks",
-		                                                      self.globalValues,
-		                                                      backendNetwork,
-		                                                      1))
-		self.sharedBackend = self.formatString(self.setItems("Networks",
-		                                                     self.globalValues,
-		                                                     sharedBackendNetwork,
-		                                                     2))
+		self.secrets = setItems(self, "Secrets",
+		                        self.stackDict)
+		self.volumes = setItems(self, "Volumes",
+		                        self.stackDict)
+		self.services = setItems(self, "Services",
+		                         self.stackDict)
+		self.domain = str(setItems(self, "Domain",
+		                           self.defaults,
+		                           domain))
+		self.email = str(setItems(self, "Email",
+		                          self.defaults,
+		                          emailAddress))
+		self.frontendNetwork = formatString(self, setItems(self, "Networks",
+		                                                   self.globalValues,
+		                                                   frontendNetwork,
+		                                                   0))
+		self.backendNetwork = formatString(self, setItems(self, "Networks",
+		                                                  self.globalValues,
+		                                                  backendNetwork,
+		                                                  1))
 		self.traefikProtocol = traefikProtocol
+		self.globals = {
+				"networks": dict(),
+				"secrets":  dict(),
+				"volumes":  dict(),
+				"version":  f'"{version}"'
+				}
 		self.oauth_port = oauth_port
-		self.puid = str(int(self.setItems("PUID", self.defaults, puid)))
-		self.pgid = str(int(self.setItems("PGID", self.defaults, pgid)))
+		self.puid = str(int(setItems(self, "PUID", self.defaults, puid)))
+		self.pgid = str(int(setItems(self, "PGID", self.defaults, pgid)))
 		self.secretsPath = str(secretsPath)
 		self.authenticatedEmailsFile = str(authenticatedEmailsFile)
 		self.authenticatedEmailsContainerPath = str(authenticatedEmailsContainerPath)
-		self.stackTitle = self.formatString(stackTitle)
-		self.vpnContainerName = self.formatString(vpnContainerName)
+		self.stackTitle = formatString(self, stackTitle)
+		self.vpnContainerName = formatString(self, vpnContainerName)
 		self.vpnHostName = self.setVPNHostname()
-		self.organizrSubdomain = self.formatString(organizrSubdomain)
+		self.organizrSubdomain = formatString(self, organizrSubdomain)
 		self.labels = labels
 		self.customResponseHeaders = self.setCustomResponseHeader(customResponseHeaders)
-		self.ip = self.parseIP()
-		self.backend_subnet = self.parseBackendSubnet()
+		self.ip = str(IP(self.defaults, self.globalValues, self.stackDict).address)
+		self.backend_subnet = str(IP(self.defaults, self.globalValues, self.stackDict).backendSubnet)
+		self.globals.update(IP(self.defaults, self.globalValues, self.stackDict).networks)
 		self.organizrURL = self.parseOrganizrFQDN()
 		self.customResponseHeadersValue = self.parseCustomResponseHeader()
 		self.customFrameOptionsValue = self.parseCustomFrameOptionsValue()
@@ -87,52 +114,13 @@ class ComposeFile(object):
 		# conditionals - move to a separate class down the line
 		self.conditionals = list()
 	
-	def setItems(self,
-	             environ,
-	             parentDict = dict(),
-	             payload = list(),
-	             index = None):
-		# CONDITIONALS
-		createParentDictionary = not parentDict
-		subdictionary = environ in parentDict and not index
-		sublist = environ in parentDict and str(index).isdigit() and 'list' in type(parentDict[environ])
-		# TODO: adjust to a master conditional map
-		if createParentDictionary:
-			parentDict = self.defaults
-		if subdictionary:
-			payload = parentDict[environ]
-		elif sublist:
-			payload = parentDict[environ][index]
-		return payload
-	
 	def setVPNHostname(self):
 		return "-".join([self.stackTitle, self.vpnContainerName])
 	
-	def parseIP(self):
-		octetsList = [
-				str(self.octet1),
-				str(self.octet2),
-				str(self.octet3)
-				]
-		payload = self.formatString(".".join(octetsList))
-		return payload
-	
 	def parseOrganizrFQDN(self):
 		domainComponentList = [self.organizrSubdomain, self.domain]
-		payload = self.formatString(".".join(domainComponentList))
-		self.formatString(payload)
-		return payload
-	
-	def formatString(self, payload):
-		payload = str(payload).lower()
-		payload = payload.replace(" ", "-")
-		payload = payload.replace("_", "-")
-		return payload
-	
-	def parseBackendSubnet(self, networkIP = 0):
-		networkIP = networkIP
-		subnetMaskComponents = [(str(self.ip) + "." + str(networkIP)), str(self.subnetMask)]
-		payload = self.formatString("/".join(subnetMaskComponents))
+		payload = formatString(self, ".".join(domainComponentList))
+		formatString(self, payload)
 		return payload
 	
 	def setCustomResponseHeader(self, customResponseHeaders):
@@ -178,5 +166,31 @@ class ComposeFile(object):
 		for k, v in self.customResponseHeaders.items():
 			value = ','.join(v)
 			headerList.append(f"{k}: {value}")
-		payload = self.formatString(",".join(headerList))
+		payload = formatString(self, ",".join(headerList))
 		return payload
+
+
+def formatString(self, payload):
+	payload = str(payload).lower()
+	payload = payload.replace(" ", "-")
+	payload = payload.replace("_", "-")
+	return payload
+
+
+def setItems(self,
+             environ,
+             parentDict = dict(),
+             payload = list(),
+             index = None):
+	# CONDITIONALS
+	createParentDictionary = not parentDict
+	subdictionary = environ in parentDict and not index
+	sublist = environ in parentDict and str(index).isdigit() and 'list' in type(parentDict[environ])
+	# TODO: adjust to a master conditional map
+	if createParentDictionary:
+		parentDict = self.defaults
+	if subdictionary:
+		payload = parentDict[environ]
+	elif sublist:
+		payload = parentDict[environ][index]
+	return payload
