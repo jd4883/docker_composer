cd1430876d22026740d2e4c154431e8195fd8da6
root <editor>
2020-03-10 01:44:23 +0000

    had a bad reference in the jinja2 template

--- templates/providers.jinja.tf
+++ templates/providers.jinja.tf
@@ -1,7 +1,7 @@
 {%- for provider in defaults %}
 {{ "provider" + provider|lower + "{" }}
 {{ "alias"|indent(2) + alias | default(provider|lower) }}
-{%- for k, v in defaults["providers"][provider].items() %}
+{%- for k, v in defaults[provider].items() %}
 {{ k|indent(2) + " = " + v }}
 {%- endfor %}
 {{ "}" }}
