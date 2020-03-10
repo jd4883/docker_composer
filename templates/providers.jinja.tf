7fccd792364ae4ba537e74a00a54d6f6c6f2301d
root <editor>
2020-03-10 02:00:52 +0000

    cleaned up formatting

--- templates/providers.jinja.tf
+++ templates/providers.jinja.tf
@@ -1,6 +1,6 @@
 {%- for provider in defaults %}
-{{ "provider" + provider|lower + "{" }}
-{{ "alias"|indent(2) + alias | default(provider|lower) }}
+{{ "provider " + provider|lower + " {" }}
+{{ "alias = "|indent(2) + alias | default(provider|lower) }}
 {%- for k, v in defaults[provider].items() %}
 {{ k|indent(2) + " = " + v|string }}
 {%- endfor %}
