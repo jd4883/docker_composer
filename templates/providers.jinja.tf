61d62bc233933b84ec40f05bd43b2efaf61d5c1e
root <editor>
2020-03-10 02:06:17 +0000

    corrected the indent syntax

--- templates/providers.jinja.tf
+++ templates/providers.jinja.tf
@@ -1,8 +1,8 @@
 {%- for provider in defaults %}
 {{ "provider " + provider|lower + " {" }}
-{{ "alias = "|indent(2) + alias | default(provider|lower) }}
+{{ "alias = "|indent(2, true) + alias | default(provider|lower) }}
 {%- for k, v in defaults[provider].items() %}
-{{ k|indent(2) + " = " + v|string }}
+{{ k|indent(2, true) + " = " + v|string }}
 {%- endfor %}
 {{ "}" }}
 
