43f38f4effc8ed7bbd4b0e4d50cd6a36c3cfba95
root <editor>
2020-03-10 02:19:29 +0000

    added logic to not make bools quote accidentally

--- templates/providers.jinja.tf
+++ templates/providers.jinja.tf
@@ -2,7 +2,11 @@
 {{ "provider " + provider|lower + " {" }}
 {{ "alias = "|indent(2, true) + alias | default(provider|lower) }}
 {%- for k, v in defaults[provider].items() %}
+{%- if v|lower == ("true" or "false") %}
+{{ k|indent(2, true) + " = " + v|string }}
+{%- else %}
 {{ k|indent(2, true) + " = \"" + v|string + "\"" }}
+{%- endif %
 {%- endfor %}
 {{ "}" }}
 
