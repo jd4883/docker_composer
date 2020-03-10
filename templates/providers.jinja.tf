5501c734599f3b12247ca7436e604b62cf4bdf37
root <editor>
2020-03-10 02:51:32 +0000

    more tweaks syntax isnt quite right

--- templates/providers.jinja.tf
+++ templates/providers.jinja.tf
@@ -2,7 +2,7 @@
 {{ "provider " + provider|lower + " {" }}
 {{ "alias = "|indent(2, true) + alias | default(provider|lower) }}
 {%- for k, v in defaults[provider].items() %}
-{%- if (v|lower is true) or (v|lower is false) %}
+{%- if v|lower == (true or false) %}
 {{ k|indent(2, true) + " = " + v|string|lower }}
 {%- else %}
 {{ k|indent(2, true) + " = \"" + v|string + "\"" }}
