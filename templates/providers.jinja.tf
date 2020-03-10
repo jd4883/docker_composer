601e9506d4f1fcfc09788dd91bb9ca281259f16e
root <editor>
2020-03-10 02:33:22 +0000

    more tweaks

--- templates/providers.jinja.tf
+++ templates/providers.jinja.tf
@@ -2,7 +2,7 @@
 {{ "provider " + provider|lower + " {" }}
 {{ "alias = "|indent(2, true) + alias | default(provider|lower) }}
 {%- for k, v in defaults[provider].items() %}
-{%- if v|lower is ("true" or "false") %}
+{%- if (v|lower == "true") or (v|lower == "false") %}
 {{ k|indent(2, true) + " = " + v|string|lower }}
 {%- else %}
 {{ k|indent(2, true) + " = \"" + v|string + "\"" }}
