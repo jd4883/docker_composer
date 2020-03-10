a22455be397660bdce720a0cd62c421676b0d04c
root <editor>
2020-03-10 02:19:58 +0000

    cleaned up bools

--- templates/providers.jinja.tf
+++ templates/providers.jinja.tf
@@ -3,7 +3,7 @@
 {{ "alias = "|indent(2, true) + alias | default(provider|lower) }}
 {%- for k, v in defaults[provider].items() %}
 {%- if v|lower == ("true" or "false") %}
-{{ k|indent(2, true) + " = " + v|string }}
+{{ k|indent(2, true) + " = " + v|string|lower }}
 {%- else %}
 {{ k|indent(2, true) + " = \"" + v|string + "\"" }}
 {%- endif %
