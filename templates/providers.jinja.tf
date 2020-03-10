6e3098b578769c0c0b3e0976bbb955fd32e91872
root <editor>
2020-03-10 01:53:24 +0000

    tightened up data typing in the template to handle bools correctly

--- templates/providers.jinja.tf
+++ templates/providers.jinja.tf
@@ -2,7 +2,7 @@
 {{ "provider" + provider|lower + "{" }}
 {{ "alias"|indent(2) + alias | default(provider|lower) }}
 {%- for k, v in defaults[provider].items() %}
-{{ k|indent(2) + " = " + v }}
+{{ k|indent(2) + " = " + v|string }}
 {%- endfor %}
 {{ "}" }}
 
