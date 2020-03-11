2046d25e661cb4d2fbc441e141fc944a4d9b2b64
root <editor>
2020-03-11 06:05:33 +0000

    still trying to get the line 10 error out

--- templates/service.jinja.tf
+++ templates/service.jinja.tf
@@ -1,5 +1,5 @@
 module {{ svc }} {
-  source = "{{ helm_module }}"
+  source = {{ helm_module|string }}
   providers = {
 {%- for provider in service.kubernetes.providers %}
     {{ provider }} = {{ provider }}.{{ provider }}
@@ -12,10 +12,10 @@ module {{ svc }} {
     max_replicas = {{ service.kubernetes.spec.max_replicas||default(defaults.kubernetes.spec.max_replicas) }}
     min_replicas = {{ service.kubernetes.spec.min_replicas|default(defaults.kubernetes.spec.min_replicas) }}
     cpu_usage = {{ service.kubernetes.spec.cpu_usage|default(defaults.kubernetes.spec.cpu_usage) }}
-    api_version = "{{ service.kubernetes.spec.api_version|default(defaults.kubernetes.spec.api_verseion) }}"
-    kind = "{{ service.kubernetes.spec.kind|default(defaults.kubernetes.spec.kind) }}"
+    api_version = {{ service.kubernetes.spec.api_version|default(defaults.kubernetes.spec.api_verseion)|string }}
+    kind = {{ service.kubernetes.spec.kind|default(defaults.kubernetes.spec.kind)|string }}
   }
-  domain = "{{ defaults.domain|default(example.com) }}"
+  domain = {{ defaults.domain|default(example.com)|string }}
   subdomains = [
 {%- if service.subdomains is defined %}
 {%- for i in service.subdomains %}
