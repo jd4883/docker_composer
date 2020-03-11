aa1654d84f1b4b3ac294d1290baaac9277f2915b
root <editor>
2020-03-11 07:24:36 +0000

    still testing

--- compose_generator.py
+++ compose_generator.py
@@ -65,9 +65,8 @@ if __name__ == "__main__":
 			parse_hostfile(composeFile.services[app], hostfile, hosts, defaults)
 			gen_setup_shell_script(stack, app, defaults, g, configs)
 			composeFile.services[app]['HOSTS'] = ",".join(hosts)
-			if app == "consul":
+			if app == "consul" and "kubernetes" in stack_dict[stack][app]:
 				print(f"kubernetes flag set for {app} make sure it still works")
-				print(stack_dict[stack])
 				print(composeFile.services)
 				gen_terraform_service_code(app, stack_dict[stack][app], defaults, configs)
 		gen_hostfile(stack_dict[stack], defaults, hostfile)
