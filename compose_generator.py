964f17f1bc5219e856887305c94bafa0fa42b4d9
root <editor>
2020-03-11 08:25:24 +0000

    adjusted conditional to be more permissive

--- compose_generator.py
+++ compose_generator.py
@@ -67,7 +67,7 @@ if __name__ == "__main__":
 			parse_hostfile(composeFile.services[app], hostfile, hosts, defaults)
 			gen_setup_shell_script(stack, app, defaults, g, configs)
 			composeFile.services[app]['HOSTS'] = ",".join(hosts)
-			if "kubernetes" in stack_dict[stack]["Services"][app]:
+			if app in stack_dict[stack]["Services"] and "kubernetes" in stack_dict[stack]["Services"][app]:
 			#try:
 				print(f"kubernetes flag set for {app} make sure it still works")
 				gen_terraform_service_code(app, stack_dict[stack]["Services"][app], defaults, configs)
