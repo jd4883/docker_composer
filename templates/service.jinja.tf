f55ada8e3105a2c16b07eecfde9e51bda6ed43c4
root <editor>
2020-03-11 08:17:15 +0000

    full template

--- templates/service.jinja.tf
+++ templates/service.jinja.tf
@@ -6,4 +6,30 @@ module {{ svc + " {" }}
 {%- endfor %}
   }
   helm_chart = {{ service.kubernetes.helm_chart|string }}
+  name = {% block svc %}
+  namespace = {% block service.kubernetes.namespace %}
+  spec = {
+    max_replicas = {{ service.kubernetes.spec.max_replicas||default(defaults.kubernetes.spec.max_replicas) }}
+    min_replicas = {{ service.kubernetes.spec.min_replicas|default(defaults.kubernetes.spec.min_replicas) }}
+    cpu_usage = {{ service.kubernetes.spec.cpu_usage|default(defaults.kubernetes.spec.cpu_usage) }}
+    api_version = {{ service.kubernetes.spec.api_version|default(defaults.kubernetes.spec.api_verseion)|string }}
+    kind = {{ service.kubernetes.spec.kind|default(defaults.kubernetes.spec.kind)|string }}
+  }
+  domain = {{ defaults.domain|default(example.com)|string }}
+  subdomains = [
+{%- if service.subdomains is defined %}
+{%- for i in service.subdomains %}
+    - "{{ i }}"
+{%- endfor %}
+{%- else %}
+  ]
+{%- endif %}
+  sets = [
+{%- for k,v in service.kubernetes.values.items() %}
+    {
+      name = "{{ k }}",
+      value = "{{ v }}"
+    }
+{%- endfor %}
+  ]
 }
