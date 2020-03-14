0f545bea281267f29334b1a8624db52209d1ab41
root <editor>
2020-03-14 04:11:03 +0000

    small test

--- templates/service.jinja.tf
+++ templates/service.jinja.tf
@@ -25,10 +25,10 @@ module {{ "consul" + " {" }}
   ]
 {%- endif %}
   sets = [
-{%- for k,v in service.kubernetes.values %}
+{%- for x in service.kubernetes.values %}
     {
-      name = "{{ k }}",
-      value = "{{ v }}"
+      name = "{{ x }}",
+      value = "{{ x }}"
     }
 {%- endfor %}
   ]
