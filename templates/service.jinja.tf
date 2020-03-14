ef53fb27e27fddafd7585e2023552180f73ec659
root <editor>
2020-03-14 05:12:14 +0000

    terraform is working quite well so far, its really basic the way its setup but thats ok by me for now

--- templates/service.jinja.tf
+++ templates/service.jinja.tf
@@ -1,19 +1,19 @@
 module {{ "consul" + " {" }}
-  source = {{ helm_module|string }}
+  source = "{{ helm_module|string }}"
   providers = {
 {%- for provider in service.kubernetes.providers %}
-{{ provider|indent(4, true) + " = " + provider + "." + provider }}
+{{ provider|indent(4, true) + " = \"" + provider + "." + provider + "\"" }}
 {%- endfor %}
   }
-  helm_chart = {{ service.kubernetes.helm_chart }}
-  name = {{ service.kubernetes.name }}
-  namespace = {{ service.kubernetes.namespace }}
+  helm_chart = "{{ service.kubernetes.helm_chart }}"
+  name = "{{ service.kubernetes.name }}"
+  namespace = "{{ service.kubernetes.namespace }}"
   spec = {
     max_replicas = {{ service.kubernetes.spec.max_replicas|default(defaults.kubernetes.spec.max_replicas) }}
     min_replicas = {{ service.kubernetes.spec.min_replicas|default(defaults.kubernetes.spec.min_replicas) }}
     cpu_usage = {{ service.kubernetes.spec.cpu_usage|default(defaults.kubernetes.spec.cpu_usage) }}
-    api_version = {{ service.kubernetes.spec.api_version|default(defaults.kubernetes.spec.api_verseion)|string }}
-    kind = {{ service.kubernetes.spec.kind|default(defaults.kubernetes.spec.kind)|string }}
+    api_version = "{{ service.kubernetes.spec.api_version|default(defaults.kubernetes.spec.api_version)|string }}"
+    kind = "{{ service.kubernetes.spec.kind|default(defaults.kubernetes.spec.kind)|string }}"
   }
   domain = {{ defaults.domain|default("example.com")|string }}
   subdomains = [
