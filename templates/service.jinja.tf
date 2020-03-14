e2dc0bf08335d0d7d3aed4b91ed10ca4deaf8379
root <editor>
2020-03-14 01:45:27 +0000

    i think i found the error, a ||wq

--- templates/service.jinja.tf
+++ templates/service.jinja.tf
@@ -9,7 +9,7 @@ module {{ "consul" + " {" }}
   name = {{ service["kubernetes"]["name"] }}
   namespace = {{ service["kubernetes"]["namespace"] }}
   spec = {
-    max_replicas = {{ service.kubernetes.spec.max_replicas||default(defaults.kubernetes.spec.max_replicas) }}
+    max_replicas = {{ service.kubernetes.spec.max_replicas|default(defaults.kubernetes.spec.max_replicas) }}
     min_replicas = {{ service.kubernetes.spec.min_replicas|default(defaults.kubernetes.spec.min_replicas) }}
     cpu_usage = {{ service.kubernetes.spec.cpu_usage|default(defaults.kubernetes.spec.cpu_usage) }}
     api_version = {{ service.kubernetes.spec.api_version|default(defaults.kubernetes.spec.api_verseion)|string }}
