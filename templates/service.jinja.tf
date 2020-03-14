9b18446979cfaf4e7f3c5e915b0520971f886c47
root <editor>
2020-03-14 01:49:50 +0000

    issue seems fixed!!!!

--- templates/service.jinja.tf
+++ templates/service.jinja.tf
@@ -5,9 +5,9 @@ module {{ "consul" + " {" }}
 {{ provider|indent(4, true) + " = " + provider + "." + provider }}
 {%- endfor %}
   }
-  helm_chart = {{ service["kubernetes"]["helm_chart"] }}
-  name = {{ service["kubernetes"]["name"] }}
-  namespace = {{ service["kubernetes"]["namespace"] }}
+  helm_chart = {{ service.kubernetes.helm_chart }}
+  name = {{ service.kubernetes.name }}
+  namespace = {{ service.kubernetes.namespace }}
   spec = {
     max_replicas = {{ service.kubernetes.spec.max_replicas|default(defaults.kubernetes.spec.max_replicas) }}
     min_replicas = {{ service.kubernetes.spec.min_replicas|default(defaults.kubernetes.spec.min_replicas) }}
@@ -15,7 +15,7 @@ module {{ "consul" + " {" }}
     api_version = {{ service.kubernetes.spec.api_version|default(defaults.kubernetes.spec.api_verseion)|string }}
     kind = {{ service.kubernetes.spec.kind|default(defaults.kubernetes.spec.kind)|string }}
   }
-  domain = {{ defaults.domain|default(example.com)|string }}
+  domain = {{ defaults.domain|default("example.com")|string }}
   subdomains = [
 {%- if service.subdomains is defined %}
 {%- for i in service.subdomains %}
