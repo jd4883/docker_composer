79a38d2a37bdceb27116d6ac123c4cb22fcf40b6
root <editor>
2020-03-15 03:02:08 +0000

    more testing

--- compose_generator.py
+++ compose_generator.py
@@ -31,7 +31,7 @@ def gen_terraform_service_code(app, app_dict, defaults, configs):
 	render = t.render(defaults = defaults,
 	                  helm_module = str(os.environ["TF_MODULE_HELM"]),
 	                  service = app_dict,
-	                  configs = configs)
+	                  configs = "${CONFIGS}")
 	f.write(render)
 
 
